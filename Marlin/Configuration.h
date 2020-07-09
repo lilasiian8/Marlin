6b177d6eb52c87a36996429b97d8c690665d7ba6
lilasiian8 <67174046+lilasiian8@users.noreply.github.com>
2020-07-08 21:32:55 -0700

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -71,7 +71,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(lilasiian8 20200708)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(lilasiian8 20200708 PM)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -980,7 +980,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -55, -13, 1 }
+#define NOZZLE_TO_PROBE_OFFSET { -55, -13, -1.20 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
